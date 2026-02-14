9e1ded7a64a3ca6f8cf8aa67b3a723076d158774
futuristic-coder <raman17100100@gmail.com>
2026-02-14 15:14:19 +0530

    fix case sensitivity

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import SignIn from "./pages/SignIn";
-import Signup from "./pages/Signup";
+import SignUp from "./pages/SignUp";
 import Customize from "./pages/Customize";
 import { userDataContext } from "./context/UserContext";
 import Home from "./pages/Home"
@@ -11,7 +11,7 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={(userData?.assistantImage && userData?.assistantName)? <Home />:<Navigate to={"/customize"}/> } />
-      <Route path="/signup" element={!userData ? <Signup /> : <Navigate to={"/"} />} />
+      <Route path="/signup" element={!userData ? <SignUp /> : <Navigate to={"/"} />} />
       <Route path="/signin" element={!userData ? <SignIn /> : <Navigate to={"/"} />} />
       <Route path="/customize" element={userData ? <Customize /> : <Navigate to={"/signup"} />} />
     </Routes>
